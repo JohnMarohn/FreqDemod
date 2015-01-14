1eeb4c9eeaa7142bf5f43e8f6e072ad979d8c79d
Ryan Dwyer <ryanpdwyer@gmail.com>
2015-01-14 15:47:29 -0500

    Updated .gitignore with .eggs/ folder now used by pip installation

--- setup.py
+++ setup.py
@@ -2,17 +2,18 @@
 # Enables the package to be used in develop mode
 # See http://pythonhosted.org/setuptools/setuptools.html#development-mode
 # See https://github.com/scikit-learn/scikit-learn/issues/1016
-try:
-    import setuptools
-except ImportError:
-    pass
-
-from distutils.core import setup
+# setuptools is included with pip; basically python standard at this point
+from setuptools import setup
 
 setup(name='FreqDemod',
       version='0.2',
       description='',
       author='John Marohn',
       author_email='jam99@cornell.edu',
-      packages=['freqdemod']
+      packages=['freqdemod'],
+      install_requires=['numpy', 'scipy', 'matplotlib', 'h5py',],
+      tests_require=['nose'],
+      zip_safe=False,
+      include_package_data=True,
+      test_suite='nose.collector',
       )
