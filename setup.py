783d3a4a0e60fa5a59d4d26467525deffef3442f
Ryan Dwyer <ryanpdwyer@gmail.com>
2015-01-23 18:10:17 -0500

    Moved LICENSE to the correct place. Deleted extra setup.py in freqdemod subdirectory. Add classifiers, description, long_description and license to setup.py

--- setup.py
+++ setup.py
@@ -3,17 +3,44 @@
 # See http://pythonhosted.org/setuptools/setuptools.html#development-mode
 # See https://github.com/scikit-learn/scikit-learn/issues/1016
 # setuptools is included with pip; basically python standard at this point
-from setuptools import setup
+import io
+
+from setuptools import setup, find_packages
+
+
+description = """This package provides functions for analyzing and plotting the
+time-dependent frequency and amplitude of a sinusoidally oscillating signal."""
+
+readme = io.open('README.rst', mode='r', encoding='utf-8').read()
+
+doclink = """
+Documentation
+-------------
+
+The full documentation is at http://FreqDemod.rtfd.org."""
+
+history = io.open('HISTORY.rst', mode='r',
+                  encoding='utf-8').read().replace('.. :changelog:', '')
 
 setup(name='FreqDemod',
       version='0.2',
-      description='',
+      description=description,
+      long_description=readme + '\n\n' + doclink + '\n\n' + history,
       author='John Marohn',
+      license='GPLv3',
       author_email='jam99@cornell.edu',
-      packages=['freqdemod'],
+      packages=find_packages(),
       install_requires=['numpy', 'scipy', 'matplotlib', 'h5py',],
       tests_require=['nose'],
       zip_safe=False,
       include_package_data=True,
       test_suite='nose.collector',
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
       )
