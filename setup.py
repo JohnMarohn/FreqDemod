4d31df6a74b24db2e01a9cadf3fee051047d24a6
Ryan Dwyer <ryanpdwyer@gmail.com>
2015-01-23 19:31:37 -0500

    Bumped version number to 0.2.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ history = io.open('HISTORY.rst', mode='r',
                   encoding='utf-8').read().replace('.. :changelog:', '')
 
 setup(name='FreqDemod',
-      version='0.2',
+      version='0.2.1',
       description=description,
       long_description=readme + '\n\n' + doclink + '\n\n' + history,
       author='John Marohn',
