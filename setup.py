f5105ed0c1cf3ece2c206b7249f0b1653980f387
Peter Sun <peterhs73@outlook.com>
2019-09-11 17:17:32 -0400

    fix the installation issue

--- setup.py
+++ setup.py
@@ -26,30 +26,34 @@ Documentation
 
 The full documentation is at https://freqdemod.rtfd.org."""
 
-history = io.open('HISTORY.rst', mode='r',
-                  encoding='utf-8').read().replace('.. :changelog:', '')
+history = (
+    io.open('HISTORY.rst', mode='r', encoding='utf-8')
+    .read()
+    .replace('.. :changelog:', '')
+)
 
-setup(name='FreqDemod',
-      version='0.3.dev1',
-      description=description,
-      long_description=readme + '\n\n' + doclink + '\n\n' + history,
-      author='John Marohn',
-      license='GPLv3',
-      author_email='jam99@cornell.edu',
-      url='https://github.com/JohnMarohn/FreqDemod',
-      packages=find_packages(),
-      install_requires=install_requires,
-      setup_requires=["setuptools_git >= 0.3"],
-      tests_require=[],
-      zip_safe=False,
-      include_package_data=True,
-      test_suite='freqdemod.tests.discover',
-      classifiers=[
+setup(
+    name='FreqDemod',
+    version='0.3.1-beta',
+    description=description,
+    long_description=readme + '\n\n' + doclink + '\n\n' + history,
+    author='John Marohn',
+    license='GPLv3',
+    author_email='jam99@cornell.edu',
+    url='https://github.com/JohnMarohn/FreqDemod',
+    packages=['freqdemod', 'freqdemod.hdf5'],
+    install_requires=install_requires,
+    setup_requires=["setuptools_git >= 0.3"],
+    tests_require=[],
+    zip_safe=False,
+    include_package_data=True,
+    test_suite='freqdemod.tests.discover',
+    classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
         'Intended Audience :: Science/Research',
         'Natural Language :: English',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
     ],
-      )
+)
